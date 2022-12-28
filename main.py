e6fb336af151a30a75496ed8e5403454ac322fb2
Omar Emad <57142758+OmarEmad313@users.noreply.github.com>
2022-12-28 13:53:06 +0200

    GUI implemented

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import sys
+from tkinter import *
 
 
+class MyWindow:
+    def __init__(self, win):
+        self.lbl1=Label(win, text="Enter text: ")
+        self.lbl3=Label(win, text='Result')
+
+        self.t1=Entry(bd=3)
+        self.t3=Entry()
+        self.btn1 = Button(win, text='Encrypt')
+        self.btn2=Button(win, text='Decrypt')
+        self.lbl1.place(x=100, y=50)
+        self.t1.place(x=200, y=50)
+        self.b1=Button(win, text='Encrypt', command=self.add)
+        self.b2=Button(win, text='Decrypt')
+        self.b2.bind('<Button-1>', self.sub)
+        self.b1.place(x=100, y=150)
+        self.b2.place(x=200, y=150)
+        self.lbl3.place(x=100, y=200)
+        self.t3.place(x=200, y=200)
+
+    def add(self):
+        self.t3.delete(0, 'end')
+        text=self.t1.get()
+        cipherText=encrypt_DES(text,key)
+        self.t3.insert(END, cipherText)
+    def sub(self, event):
+        self.t3.delete(0, 'end')
+        cipherText=self.t1.get()
+        text=decrypt_DES(cipherText,key)
+        self.t3.insert(END, text)
+
+window=Tk()
+mywin=MyWindow(window)
+window.title('CBC with DES')
+window.geometry("400x300+10+10")
+window.mainloop()
 # Hexadecimal to binary conversion
 def hex2bin(s):
     mp = {'0': "0000",
